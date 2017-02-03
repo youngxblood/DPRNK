2011d81e12a12b19f11c879df9413b73cd2d58f9
youngxblood <crazyeight68@gmail.com>
2017-02-03 00:21:11 -0800

    Test
    Testing pulling.

---
+++ moneyInTheBank/src/Car.java
@@ -0,0 +1,16 @@
+/**
+ * Just a car
+ */
+public class Car {
+    private static final int SPEED = 1;
+    private int mybrakeSpeed;
+
+    public Car () {
+      mybrakeSpeed = 1;
+    }
+    public void setBrakeSpeed(int theSpeed) {
+        mybrakeSpeed = theSpeed;
+    }
+
+
+}
